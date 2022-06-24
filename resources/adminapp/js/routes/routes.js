407ea31c5650598b8f4fb1b8919733d461020206
Laravel Daily <info@laraveldaily.com>
2022-06-24 04:17:41 +0000

    QuickAdminPanel automatic commit

--- resources/adminapp/js/routes/routes.js
+++ resources/adminapp/js/routes/routes.js
@@ -111,86 +111,6 @@ const routes = [
         ]
       },
       {
-        path: 'user-management',
-        name: 'user_management',
-        component: View,
-        redirect: { name: 'permissions.index' },
-        children: [
-          {
-            path: 'permissions',
-            name: 'permissions.index',
-            component: () => import('@cruds/Permissions/Index.vue'),
-            meta: { title: 'cruds.permission.title' }
-          },
-          {
-            path: 'permissions/create',
-            name: 'permissions.create',
-            component: () => import('@cruds/Permissions/Create.vue'),
-            meta: { title: 'cruds.permission.title' }
-          },
-          {
-            path: 'permissions/:id',
-            name: 'permissions.show',
-            component: () => import('@cruds/Permissions/Show.vue'),
-            meta: { title: 'cruds.permission.title' }
-          },
-          {
-            path: 'permissions/:id/edit',
-            name: 'permissions.edit',
-            component: () => import('@cruds/Permissions/Edit.vue'),
-            meta: { title: 'cruds.permission.title' }
-          },
-          {
-            path: 'roles',
-            name: 'roles.index',
-            component: () => import('@cruds/Roles/Index.vue'),
-            meta: { title: 'cruds.role.title' }
-          },
-          {
-            path: 'roles/create',
-            name: 'roles.create',
-            component: () => import('@cruds/Roles/Create.vue'),
-            meta: { title: 'cruds.role.title' }
-          },
-          {
-            path: 'roles/:id',
-            name: 'roles.show',
-            component: () => import('@cruds/Roles/Show.vue'),
-            meta: { title: 'cruds.role.title' }
-          },
-          {
-            path: 'roles/:id/edit',
-            name: 'roles.edit',
-            component: () => import('@cruds/Roles/Edit.vue'),
-            meta: { title: 'cruds.role.title' }
-          },
-          {
-            path: 'users',
-            name: 'users.index',
-            component: () => import('@cruds/Users/Index.vue'),
-            meta: { title: 'cruds.user.title' }
-          },
-          {
-            path: 'users/create',
-            name: 'users.create',
-            component: () => import('@cruds/Users/Create.vue'),
-            meta: { title: 'cruds.user.title' }
-          },
-          {
-            path: 'users/:id',
-            name: 'users.show',
-            component: () => import('@cruds/Users/Show.vue'),
-            meta: { title: 'cruds.user.title' }
-          },
-          {
-            path: 'users/:id/edit',
-            name: 'users.edit',
-            component: () => import('@cruds/Users/Edit.vue'),
-            meta: { title: 'cruds.user.title' }
-          }
-        ]
-      },
-      {
         path: 'data-master',
         name: 'data_master',
         component: View,
@@ -321,6 +241,86 @@ const routes = [
             name: 'kampus.edit',
             component: () => import('@cruds/Kampus/Edit.vue'),
             meta: { title: 'cruds.kampu.title' }
+          }
+        ]
+      },
+      {
+        path: 'user-management',
+        name: 'user_management',
+        component: View,
+        redirect: { name: 'permissions.index' },
+        children: [
+          {
+            path: 'permissions',
+            name: 'permissions.index',
+            component: () => import('@cruds/Permissions/Index.vue'),
+            meta: { title: 'cruds.permission.title' }
+          },
+          {
+            path: 'permissions/create',
+            name: 'permissions.create',
+            component: () => import('@cruds/Permissions/Create.vue'),
+            meta: { title: 'cruds.permission.title' }
+          },
+          {
+            path: 'permissions/:id',
+            name: 'permissions.show',
+            component: () => import('@cruds/Permissions/Show.vue'),
+            meta: { title: 'cruds.permission.title' }
+          },
+          {
+            path: 'permissions/:id/edit',
+            name: 'permissions.edit',
+            component: () => import('@cruds/Permissions/Edit.vue'),
+            meta: { title: 'cruds.permission.title' }
+          },
+          {
+            path: 'roles',
+            name: 'roles.index',
+            component: () => import('@cruds/Roles/Index.vue'),
+            meta: { title: 'cruds.role.title' }
+          },
+          {
+            path: 'roles/create',
+            name: 'roles.create',
+            component: () => import('@cruds/Roles/Create.vue'),
+            meta: { title: 'cruds.role.title' }
+          },
+          {
+            path: 'roles/:id',
+            name: 'roles.show',
+            component: () => import('@cruds/Roles/Show.vue'),
+            meta: { title: 'cruds.role.title' }
+          },
+          {
+            path: 'roles/:id/edit',
+            name: 'roles.edit',
+            component: () => import('@cruds/Roles/Edit.vue'),
+            meta: { title: 'cruds.role.title' }
+          },
+          {
+            path: 'users',
+            name: 'users.index',
+            component: () => import('@cruds/Users/Index.vue'),
+            meta: { title: 'cruds.user.title' }
+          },
+          {
+            path: 'users/create',
+            name: 'users.create',
+            component: () => import('@cruds/Users/Create.vue'),
+            meta: { title: 'cruds.user.title' }
+          },
+          {
+            path: 'users/:id',
+            name: 'users.show',
+            component: () => import('@cruds/Users/Show.vue'),
+            meta: { title: 'cruds.user.title' }
+          },
+          {
+            path: 'users/:id/edit',
+            name: 'users.edit',
+            component: () => import('@cruds/Users/Edit.vue'),
+            meta: { title: 'cruds.user.title' }
           }
         ]
       }
